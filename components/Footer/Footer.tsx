037294a1e6345f5e8575260e625399c36ac77eb4
Andrii Butenko <andrejbutik@gmail.com>
2025-11-05 19:38:36 +0100

    Finished css footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,47 +1,53 @@
 import Link from "next/link";
 import css from "./Footer.module.css";
+
 export default function Footer() {
   return (
     <section className={css.section}>
       <div className={`container ${css.containerFooter}`}>
-        <div className={css.linksContainer}>
-          <a href="" aria-label="На головну">
-            <svg width="84" height="36" aria-hidden="true">
-              <use href="/symbol-defs.svg#icon-company-logo"></use>
-            </svg>
-          </a>
-          <div className={css.navigationList}>
-            <h2 className={css.menu}>Меню</h2>
-            <ul className={css.navigationList}>
-              <li className={css.navigation}>
-                <a href="">Головна</a>
-              </li>
-              <li className={css.navigation}>
-                <a href="">Товари</a>
-              </li>
-              <li className={css.navigation}>
-                <a href="">Категорії</a>
-              </li>
-            </ul>
+        <div className={css.containerWrap}>
+          <div className={css.linksContainer}>
+            <a href="" aria-label="На головну" className={css.logo}>
+              <svg width="84" height="36" aria-hidden="true">
+                <use href="/symbol-defs.svg#icon-company-logo"></use>
+              </svg>
+            </a>
+            <div className={css.navigationList}>
+              <h2 className={css.menu}>Меню</h2>
+              <ul className={css.navigationList}>
+                <li className={css.navigation}>
+                  <a href="">Головна</a>
+                </li>
+                <li className={css.navigation}>
+                  <a href="">Товари</a>
+                </li>
+                <li className={css.navigation}>
+                  <a href="">Категорії</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className={css.subscribeContainer}>
+            <h3 className={css.subscribe}>Підписатися</h3>
+            <p className={css.text}>
+              Приєднуйтесь до нашої розсилки, щоб бути в курсі новин та акцій.
+            </p>
+            <div className={css.containerSubscribe}>
+              <form action="" className={css.containerSubscribe}>
+                <input
+                  type="mail"
+                  placeholder="Введіть ваш email"
+                  className={css.input}
+                  pattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+                />
+                <button type="submit" className={css.button}>
+                  Підписатися
+                </button>
+              </form>
+            </div>
           </div>
         </div>
-        <div className={css.subscribeContainer}>
-          <h3 className={css.subscribe}>Підписатися</h3>
-          <p className={css.text}>
-            Приєднуйтесь до нашої розсилки, щоб бути в курсі новин та акцій.
-          </p>
-          <div className={css.containerSubscribe}>
-            <input
-              type="mail"
-              placeholder="Введіть ваш email"
-              className={css.input}
-              pattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
-            />
-            <button type="submit" className={css.button}>
-              Підписатися
-            </button>
-          </div>
-        </div>
+
         <div className={css.socials}>
           <p className={css.rights}>
             {new Date().getFullYear()} Clothica. Всі права захищені.
@@ -52,6 +58,7 @@ export default function Footer() {
                 href="https://www.facebook.com"
                 aria-label="facebook"
                 target="_blank"
+                className={css.socialLinks}
               >
                 <svg width="32" height="32" aria-hidden="true">
                   <use href="/symbol-defs.svg#icon-facebook"></use>
@@ -63,6 +70,7 @@ export default function Footer() {
                 href="https://www.instagram.com"
                 aria-label="instagram"
                 target="_blank"
+                className={css.socialLinks}
               >
                 <svg width="32" height="32" aria-hidden="true">
                   <use href="/symbol-defs.svg#icon-instagram"></use>
@@ -70,7 +78,12 @@ export default function Footer() {
               </Link>
             </li>
             <li>
-              <Link href="https://x.com" aria-label="x" target="_blank">
+              <Link
+                href="https://x.com"
+                aria-label="x"
+                target="_blank"
+                className={css.socialLinks}
+              >
                 <svg width="32" height="32" aria-hidden="true">
                   <use href="/symbol-defs.svg#icon-x"></use>
                 </svg>
@@ -81,6 +94,7 @@ export default function Footer() {
                 href="https://www.youtube.com"
                 aria-label="youtube"
                 target="_blank"
+                className={css.socialLinks}
               >
                 <svg width="32" height="32" aria-hidden="true">
                   <use href="/symbol-defs.svg#icon-youtube"></use>
