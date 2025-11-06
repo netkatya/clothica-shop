bd5fce3de324d19397e17305045e2f8ebcb54374
Vladyslav Korniienko <vladyslav.korniienko@stud.onu.edu.ua>
2025-11-06 17:26:12 +0200

    add style section

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import Reviews from "@/components/Reviews/Reviews";
+import Style from "@/components/Style/Style";
 
 export default function Home() {
   return (
     <main>
+      <Style />
       <Reviews />
     </main>
   );
