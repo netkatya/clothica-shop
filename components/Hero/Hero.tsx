01daeeb03aa307e134ca127049bdfe5f1174227d
Ievgen Prygoda <e.klimochenkova@gmail.com>
2025-11-08 14:01:31 +0000

    hero fixed

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -6,40 +6,76 @@ import { useMediaQuery } from 'react-responsive';
 import { useState, useEffect } from 'react';
 
 const Hero = () => {
+  const [isClient, setIsClient] = useState(false);
 
-    const [isClient, setIsClient] = useState(false);
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
 
-    useEffect(() => {
-        setIsClient(true);
-    }, []);
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1439 });
+  const isDesktop = useMediaQuery({ minWidth: 1440 });
 
-    const isMobile = useMediaQuery({ maxWidth: 767 });
-    const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1439 });
-    const isDesktop = useMediaQuery({ minWidth: 1440 });
-
-    return (
-        <section className={css.hero}>
-            <div className={css.container}>
-                <div className={css.hero_content}>
-                    <div className={css.text_wrapper}>
-                        <h1 className={css.hero_title}>Знайди свій стиль з Clothica вже сьогодні!</h1>
-                        <p className={css.hero_text}>Clothica — це місце, де комфорт поєднується зі стилем. Ми створюємо базовий одяг, який легко комбінується та підходить для будь-якої нагоди. Обирай речі, що підкреслять твою індивідуальність і завжди будуть актуальними.</p>
-                    </div>
-                    <div className={css.hero_links}>
-                        <a href="#PopularGoods" className={css.link_primary}>До товарів</a>
-                        <a href="#PopularCategories" className={css.link_secondary}>Дослідити категорії</a>
-                    </div>
-                </div>
-                {isClient &&
-                <>
-                    {isMobile && <Image src="/img/hero/hero-image-mobile.png" alt="Hero Image" width={335} height={335} className={css.hero_image} />}
-                    {isTablet &&<Image src="/img/hero/hero-image-tablet.png" alt="Hero Image" width={336} height={425} className={css.hero_image} />}
-                    {isDesktop &&<Image src="/img/hero/hero-image-desktop.png" alt="Hero Image" width={640} height={394} className={css.hero_image} />}
-                </>
-                }
+  return (
+    <section className={css.hero}>
+      <div className="container">
+        <div className={css.all_hero_content}>
+          <div className={css.hero_content}>
+            <div className={css.text_wrapper}>
+              <h1 className={css.hero_title}>
+                Знайди свій стиль з Clothica вже сьогодні!
+              </h1>
+              <p className={css.hero_text}>
+                Clothica — це місце, де комфорт поєднується зі стилем. Ми
+                створюємо базовий одяг, який легко комбінується та підходить для
+                будь-якої нагоди. Обирай речі, що підкреслять твою
+                індивідуальність і завжди будуть актуальними.
+              </p>
             </div>
-        </section>
-    );
-}
+            <div className={css.hero_links}>
+              <a href="#PopularGoods" className={css.link_primary}>
+                До товарів
+              </a>
+              <a href="#PopularCategories" className={css.link_secondary}>
+                Дослідити категорії
+              </a>
+            </div>
+          </div>
+          {isClient && (
+            <>
+              {isMobile && (
+                <Image
+                  src="/img/hero/hero-image-mobile.png"
+                  alt="Hero Image"
+                  width={335}
+                  height={335}
+                  className={css.hero_image}
+                />
+              )}
+              {isTablet && (
+                <Image
+                  src="/img/hero/hero-image-tablet.png"
+                  alt="Hero Image"
+                  width={336}
+                  height={425}
+                  className={css.hero_image}
+                />
+              )}
+              {isDesktop && (
+                <Image
+                  src="/img/hero/hero-image-desktop.png"
+                  alt="Hero Image"
+                  width={640}
+                  height={394}
+                  className={css.hero_image}
+                />
+              )}
+            </>
+          )}
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Hero;
