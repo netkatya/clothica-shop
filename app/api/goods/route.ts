160f6ad91484ceeb6206f35d295ca458c91190a4
Viktor Liulchyk <viktor.lyulchik@gmail.com>
2025-11-09 15:35:52 +0200

    add types&constants&routes&selects 4 categories&goods

---
+++ app/api/goods/route.ts
@@ -0,0 +1,39 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { api } from '../api';
+
+import { isAxiosError } from 'axios';
+
+export async function GET(request: NextRequest) {
+  try {
+    const page = Number(request.nextUrl.searchParams.get('page') ?? 1);
+    const perPage = Number(request.nextUrl.searchParams.get('perPage') ?? 12);
+    const gender = request.nextUrl.searchParams.get('gender') ?? '';
+    const size = request.nextUrl.searchParams.get('size') ?? '';
+    const minPrice = request.nextUrl.searchParams.get('minPrice') ?? '';
+    const maxPrice = request.nextUrl.searchParams.get('maxPrice') ?? '';
+    const res = await api('/api/goods', {
+      params: {
+        page,
+        perPage,
+        ...(gender && { gender }),
+        ...(size && { size: size.split(',') }),
+        ...(minPrice && { minPrice }),
+        ...(maxPrice && { maxPrice }),
+      },
+    });
+
+    return NextResponse.json(res.data, { status: res.status });
+  } catch (error) {
+    if (isAxiosError(error)) {
+      return NextResponse.json(
+        { error: error.message, response: error.response?.data },
+        { status: error.response?.status ?? 500 }
+      );
+    }
+
+    return NextResponse.json(
+      { error: 'Internal Server Error' },
+      { status: 500 }
+    );
+  }
+}
--- lib/api/clientApi.ts
+++ lib/api/clientApi.ts
@@ -1,3 +1,4 @@
+import { isAxiosError } from 'axios';
 import { nextServer } from './api';
 import {
   LoginRequest,
@@ -7,6 +8,8 @@ import {
   UpdateRequest,
 } from '@/types/auth';
 import { User } from '@/types/user';
+import { Gender, Good, Size } from '@/types/goods';
+import { Category } from '@/types/categoty';
 
 export const register = async (data: RegisterRequest) => {
   const res = await nextServer.post<User>('/auth/register', data);
@@ -56,4 +59,94 @@ export async function updateMeAvatar(update: File): Promise<User> {
     dataFile
   );
   return data;
+}
+
+export interface FetchGoodsResponse {
+  data: Good[];
+  totalPage: number;
+}
+
+export interface FetchGoodsParam {
+  page: string;
+  perPage: string;
+  gender?: Gender;
+  size?: Size[];
+  minPrice?: string;
+  maxPrice?: string;
+}
+
+export interface FetchCategoriesResponse {
+  data: Category[];
+  totalPage: number;
+}
+
+export interface FetchCategoriesParam {
+  page: string;
+  perPage: string;
+}
+
+export async function fetchGoodsClient(
+  page = 1,
+  perPage = 12,
+  gender?: Gender,
+  size?: Size[],
+  minPrice?: number,
+  maxPrice?: number
+): Promise<FetchGoodsResponse> {
+  try {
+    const params: FetchGoodsParam = {
+      page: String(page),
+      perPage: String(perPage),
+    };
+    if (gender) params.gender = gender;
+    if (size) params.size = size;
+    if (minPrice) params.minPrice = String(minPrice);
+    if (maxPrice) params.maxPrice = String(maxPrice);
+
+    const { data } = await nextServer.get<FetchGoodsResponse>('/goods', {
+      params,
+    });
+    return data;
+  } catch (error) {
+    if (isAxiosError(error)) {
+      throw new Error(error.response?.data?.message || 'Fetching goods failed');
+    }
+    throw new Error('Fetching goods failed');
+  }
+}
+
+export async function fetchGoodById(id: string): Promise<Good> {
+  try {
+    const { data } = await nextServer.get<Good>(`/goods/${id}`);
+    return data;
+  } catch (error) {
+    if (isAxiosError(error)) {
+      throw new Error(error.response?.data?.message || 'Fetching good failed');
+    }
+    throw new Error('Fetching good failed');
+  }
+}
+
+export async function fetchCategoriesClient(
+  page = 1,
+  perPage = 6
+): Promise<FetchCategoriesResponse> {
+  try {
+    const params: FetchCategoriesParam = {
+      page: String(page),
+      perPage: String(perPage),
+    };
+
+    const { data } = await nextServer.get<FetchGoodsResponse>('/categories', {
+      params,
+    });
+    return data;
+  } catch (error) {
+    if (isAxiosError(error)) {
+      throw new Error(
+        error.response?.data?.message || 'Fetching categories failed'
+      );
+    }
+    throw new Error('Fetching categories failed');
+  }
 }
